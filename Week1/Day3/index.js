08e4bf00e143559e92617b1874fa6488c353adb3
Eric Sang <55461971+Eric-R-Sang@users.noreply.github.com>
2023-05-31 12:16:41 +0200

    Day3 homework

---
+++ Week1/Day3/index.js
@@ -0,0 +1,13 @@
+function fib(n) {
+    let curr = 1;
+    let prev = 0;
+    console.log(prev);
+    for (let i = 1; i < n; ++i) {
+        console.log(curr);
+        let temp = prev;
+        prev = curr;
+        curr += temp;
+    }
+}
+
+fib(10);
