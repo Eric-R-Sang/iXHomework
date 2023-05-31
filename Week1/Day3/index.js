cb0bd9a57a6522ec92ada067a3de17b2576b2edc
JoelOniha <134166393+JoelOniha@users.noreply.github.com>
2023-05-31 13:23:52 +0200

    joel

--- Week1/Day3/index.js
+++ Week1/Day3/index.js
@@ -12,3 +12,16 @@ function fib(n) {
 }
 
 fib(10);
+
+// Joel's implementation
+function printFirstTen(){
+    let sequence = [0,1];
+    console.log(sequence[0]);
+    console.log(sequence[1]);
+    for(let i = 2; i < 10; ++i){
+        sequence[i]= sequence[i-1] + sequence[i-2];
+        console.log(sequence[i]); 
+    }
+}
+printFirstTen();
+    
